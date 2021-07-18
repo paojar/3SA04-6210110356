683e692ea618de88c218a151c9a4eeab903842d7
paojar <6210110356@psu.ac.th>
2021-07-18 13:08:37 +0700

    Navigation

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ZipCodeScreen from './screen/ZipCodeScreen';
+import WeatherScreen from './screen/WeatherScreen';
 
 const Stack = createStackNavigator();
 export default function App() {
  return (
  <NavigationContainer>
    <Stack.Navigator>
-      <Stack.Screen name="Home" component={ZipCodeScreen} />
+      <Stack.Screen name="Home" component={ZipCodeScreen}/>
+      <Stack.Screen name="Weather" component={WeatherScreen}/>
    </Stack.Navigator>
  </NavigationContainer>
  );
--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FlatList,View,Text,StyleSheet } from 'react-native'
+import { FlatList,View,Text,StyleSheet, TouchableHighlight } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -9,23 +10,27 @@ const availableZipItems = [
     { place: 'Chonburi', code: '20000' },
 ]
 
-const ZipItem = ({place, code}) => (
-    <View style={styles.zipItem}>
-    <Text>{place}</Text>
-    <Text>{code}</Text>
-    </View>
+const ZipItem = ({place, code, navigation}) => (
+    <TouchableHighlight onPress={() =>{
+        navigation.navigate('Weather',{zipCode:code})
+    }}>
+        <View style={styles.zipItem}>
+            <Text>{place}</Text>
+            <Text>{code}</Text>
+        </View> 
+    </TouchableHighlight>
 )
 
 //const _keyExtractor = item => item.code
 
 export default function ZipCodeScreen(){
-    //const navigation = useNavigation()
+    const navigation = useNavigation()
     return (
         //<View>
         <FlatList
             data={availableZipItems}
             keyExtractor={item => item.code}
-            renderItem={({item}) => <ZipItem {...item} />}
+            renderItem={({item}) => <ZipItem {...item} navigation={navigation} />}
         />
         //<StatusBar style="auto" />
         //</View>
